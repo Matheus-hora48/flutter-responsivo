51abb491dad3871fa6799b2b42f14e10660b4e65
Matheus Hora <matheus11hora@gmail.com>
2022-10-14 16:57:42 -0300

    Novos

---
+++ desafio/lib/home/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        appBar: AppBar(
+          
+        ),
+      );
+    });
+  }
+}
